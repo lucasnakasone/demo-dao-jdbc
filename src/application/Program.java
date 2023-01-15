9eda650c5f9606b6155eeda41211454da77c5727
Lucas Nakasone <lucasnakasone@hotmail.com>
2023-01-15 13:47:55 -0300

    Class created - Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department d = new Department(1, "d");
 		System.out.println(d);
+		Seller s = new Seller(1, "L", "L@a", new Date(), 3., d);
+		System.out.println(s);
 	}
 }

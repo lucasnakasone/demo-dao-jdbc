88fe30c6a5fd52fc03b3294913780ec40c478e34
Lucas Nakasone <lucasnakasone@hotmail.com>
2023-01-16 23:58:28 -0300

    Implemented - insert department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,16 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
+		/* SellerDao Tests
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("\tfindById");
@@ -25,21 +26,33 @@ public class Program {
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
 		
-		/*
 		System.out.println("\tinsert");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, d);
 		sellerDao.insert(newSeller);
 		System.out.println("Seller inserted. ID: " + newSeller.getId());
-		*/
 		
 		System.out.println("\tupdate");
 		System.out.println();
 		s = sellerDao.findById(1);
 		s.setName("Martha Waine");
 		sellerDao.update(s);
-		System.out.println("Update completed.");
+		System.out.print("Update completed.");
 		
 		System.out.println("\tdelete");
 		sellerDao.deleteById(14);
+		*/
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		/*
+		System.out.println("\tinsert department");
+		Department d = new Department(null, "Furnitures");
+		departmentDao.insert(d);
+		System.out.println("Department inserted. ID: " + d.getId());
+		*/
+		
+		
+	
+	
 	}
 }
